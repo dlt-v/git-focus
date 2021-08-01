54442003ab30c892ce55494f6844049426a6428e
tmm-michalski <tmm.michalski@gmail.com>
2021-08-01 18:32:02 +0200

    Add input change handling

--- src/AddTaskView.js
+++ src/AddTaskView.js
@@ -1,9 +1,26 @@
-import React from 'react';
-import { NavButton } from './NavButton.js';
+import React from "react";
+import { NavButton } from "./NavButton.js";
 
 export class AddTaskView extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            name: "",
+            date: "",
+            time: "",
+            flag: "",
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(event) {
+        const target = event.target;
+        const value =
+            target.type === "checkbox" ? target.checked : target.value;
+        const name = target.name;
+        console.log(value);
+        this.setState({
+            [name]: value,
+        });
     }
     render() {
         return (
@@ -14,23 +31,37 @@ export class AddTaskView extends React.Component {
                 <h1>Add Task</h1>
                 <div className="input-row">
                     <label htmlFor="task">Name:</label>
-                    <input type="text" name="task" />
+                    <input
+                        type="text"
+                        name="task"
+                        onChange={this.handleChange}
+                    />
                 </div>
                 <div className="input-row">
                     <label htmlFor="date">Date:</label>
-                    <input type="date" name="date" id="date" />
+                    <input
+                        type="date"
+                        name="date"
+                        id="date"
+                        onChange={this.handleChange}
+                    />
                 </div>
                 <div className="input-row">
                     <label htmlFor="date">Hour:</label>
-                    <input type="time" name="date" id="date" />
+                    <input
+                        type="time"
+                        name="date"
+                        id="date"
+                        onChange={this.handleChange}
+                    />
                 </div>
                 <div className="input-row">
                     <label htmlFor="flag">Flag</label>
-                    <select name="flag" id="flag">
+                    <select name="flag" id="flag" onChange={this.handleChange}>
                         <option value="red">red</option>
-                        <option value="red">blue</option>
-                        <option value="red">green</option>
-                        <option value="red">yellow</option>
+                        <option value="blue">blue</option>
+                        <option value="green">green</option>
+                        <option value="yellow">yellow</option>
                     </select>
                 </div>
                 <button>Add</button>
